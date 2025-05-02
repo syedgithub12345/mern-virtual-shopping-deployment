b50cb05198cc4fd51f3de65f2d10c87f1cf2084b
Syed Safi <syedisaf@gmail.com>
2025-05-02 16:43:45 +0530

    proj

--- client/src/store/shop/order-slice/index.js
+++ client/src/store/shop/order-slice/index.js
@@ -25,7 +25,7 @@ export const capturePayment = createAsyncThunk(
   "/order/capturePayment",
   async ({ paymentId, payerId, orderId }) => {
     const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/shop/order/capture`,
+      "${import.meta.env.VITE_API_URL}/api/shop/order/capture",
       {
         paymentId,
         payerId,
